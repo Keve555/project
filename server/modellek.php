b521f53088a09446271a353aafcae3d8a05eeefa
John Doe <johndoe@example.com>
2023-02-22 11:57:11 +0100

    jaa

--- server/modellek.php
+++ server/modellek.php
@@ -1,7 +1,7 @@
 <?php
 require_once "db.php";
 extract($_GET);
-$sql="select id, nev, kategoria from modellek order by nev asc;";
+$sql="select id, nev, kategoria from modellek order by nev asc";
 $stmt=$db->query($sql);
 echo json_encode($stmt->fetchAll());
 ?>
